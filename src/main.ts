69f2531e43f8f19def127b9e87593d6d9ca7c0a6
camilo <camilo.castro.venegas@gmail.com>
2025-01-30 16:27:01 -0300

    fixs

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,12 @@ async function bootstrap() {
 
   // Habilitar CORS globalmente, incluyendo WebSockets
   app.enableCors({
-    origin: 'http://localhost:4200',  // Permitir solicitudes desde el frontend
+    origin: [
+      'http://localhost:4200', // Para desarrollo local
+      'http://192.168.1.94:4200',    // Dirección pública de tu frontend
+      'http://172.27.64.1:4200',    // Dirección pública de tu frontend
+      
+    ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],  // Métodos permitidos
     allowedHeaders: ['Content-Type', 'Authorization'],  // Cabeceras permitidas
     credentials: true,  // Permitir el envío de cookies/credenciales
@@ -15,7 +20,7 @@ async function bootstrap() {
   // Configurar WebSocket para aceptar CORS
   app.useWebSocketAdapter(new (require('@nestjs/platform-socket.io').IoAdapter)(app));
 
-  await app.listen(3000);
+  await app.listen(3000, '0.0.0.0');
 }
 
 bootstrap();
